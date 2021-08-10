9671cabc1740d175e9f7c9a6d40627202476f33e
alirionx <dquilitzsch@outlook.de>
2021-08-11 00:41:15 +0200

    scsc

--- src/webapp/knecht.py
+++ src/webapp/knecht.py
@@ -1,9 +1,10 @@
-from main import db, User
+from main import db, User, Roles
 
 myUser = User.query.filter_by(email="dquilitzsch@outlook.de").first()
 myUser.generate_password_hash("Oviss1234!")
-
 print(myUser.password_hash)
+
+usrRole = Roles.query.filter_by(name="user").first()
 
 res = myUser.verify_password("Oviss1234!")
 print(res)
